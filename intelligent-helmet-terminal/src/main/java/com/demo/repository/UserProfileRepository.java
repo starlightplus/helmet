f6fbdd83e0f383fbf23213438fa0362c0f755adb
star <2668379109@qq.com>
2026-06-14 02:27:11 +0800

    1.3

--- intelligent-helmet-terminal/src/main/java/com/demo/repository/UserProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.repository;
-
-import com.demo.model.UserProfile;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserProfileRepository extends JpaRepository<UserProfile, Long> {
-    Optional<UserProfile> findByUserId(Long userId);
-}
